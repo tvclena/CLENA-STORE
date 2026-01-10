e37adbf97353dd6a6282506c14eb2ea8445d83ae
CLENA TV <tvclena@gmail.com>
2026-01-10 17:39:21 -0300

    Update create-pagamento.js

--- api/create-pagamento.js
+++ api/create-pagamento.js
@@ -19,6 +19,7 @@ export default async function handler(req, res) {
     return res.status(200).end();
   }
 
+  
   if (req.method !== "POST") {
     return res.status(405).json({ error: "Método não permitido" });
   }
