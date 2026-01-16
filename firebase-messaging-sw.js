88e6a84ca4ffd7561f1f94acc80245c136aa63b0
CLENA TV <tvclena@gmail.com>
2026-01-16 12:02:29 -0300

    Create firebase-messaging-sw.js

---
+++ firebase-messaging-sw.js
@@ -0,0 +1,23 @@
+importScripts('https://www.gstatic.com/firebasejs/10.7.1/firebase-app-compat.js');
+importScripts('https://www.gstatic.com/firebasejs/10.7.1/firebase-messaging-compat.js');
+
+firebase.initializeApp({
+  apiKey: "AIzaSyC_xFOKDqXRe1r-sjj-Juo6-nDfYceICTI",
+  authDomain: "agenda-facil-77785.firebaseapp.com",
+  projectId: "agenda-facil-77785",
+  storageBucket: "agenda-facil-77785.appspot.com",
+  messagingSenderId: "1029015721724",
+  appId: "1:1029015721724:web:980a9f6e0cebd702976979"
+});
+
+const messaging = firebase.messaging();
+
+messaging.onBackgroundMessage(payload => {
+  self.registration.showNotification(
+    payload.notification.title,
+    {
+      body: payload.notification.body,
+      icon: '/icon-192.png'
+    }
+  );
+});
