9ca0cf9993445ed716560a0b6b7496ebdfb66922
CLENA TV <tvclena@gmail.com>
2026-01-17 13:42:13 +0000

    Update create-subscription.js

--- api/create-subscription.js
+++ api/create-subscription.js
@@ -6,7 +6,6 @@ const supabase = createClient(
   process.env.SUPABASE_SERVICE_ROLE
 );
 
-// 💳 Mercado Pago
 const mp = new MercadoPagoConfig({
   accessToken: process.env.MP_ACCESS_TOKEN_DONO,
 });
