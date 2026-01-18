db3cf09c5484f0073035f4ea3373f09d60c913f1
CLENA TV <tvclena@gmail.com>
2026-01-18 01:40:00 -0300

    Update update-agendamento.js

--- api/update-agendamento.js
+++ api/update-agendamento.js
@@ -8,7 +8,6 @@ const supabase = createClient(
 
 export default async function handler(req, res) {
 
-  /* ================= CORS ================= */
   res.setHeader("Access-Control-Allow-Origin", "*");
   res.setHeader("Access-Control-Allow-Methods", "POST, OPTIONS");
   res.setHeader("Access-Control-Allow-Headers", "Content-Type");
